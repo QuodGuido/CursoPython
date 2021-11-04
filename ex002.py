e8fe22369151a2c2baa6bb6f2948fe89b8074f5f
QuodGuido <50514473+QuodGuido@users.noreply.github.com>
2021-11-04 07:37:19 -0300

    Ex002

---
+++ ex002.py
@@ -0,0 +1,8 @@
+print('='*20)
+print('Desafio 002\nCrie um programa que leio o nome de uma pessoa e mostre uma mensagem de boas-vindas')
+print('='*20)
+
+nome = input('Qual seu nome? ')
+
+print('Meu nome é: {}'.format(nome))
+print('='*20)
