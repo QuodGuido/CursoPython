c882bbd1d5095cfefe0edc858f254401af9d2cb7
QuodGuido <50514473+QuodGuido@users.noreply.github.com>
2021-11-04 07:47:55 -0300

    ex003

---
+++ ex003.py
@@ -0,0 +1,11 @@
+
+print('='*20)
+print('Desafio 003\nCrie um programa que leia dois números e mostre a soma entre eles')
+print('='*20)
+
+num1 = float (input('Escolha o primeiro número: '))
+num2 = float (input('Escolha o segundo número: '))
+s = num1 + num2
+
+print('A somatoria de {} + {} é igual a {}'.format(num1 , num2, s))
+print('='*20)
