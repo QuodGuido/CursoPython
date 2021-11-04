6c159d8fac6d7e4f26433d00bccf4c103d9b5c5f
QuodGuido <50514473+QuodGuido@users.noreply.github.com>
2021-11-04 08:52:34 -0300

    ex014

---
+++ ex014.py
@@ -0,0 +1,9 @@
+print('='*20)
+print('Desafio 0014\nEscreva um programa que converta uma temperatra digitada em ºC e converta para ºF.')
+print('='*20)
+
+c = float (input('A temperatura atual é: '))
+f = 9 * c / 5 + 32
+print('A tempera de {}ºC corresponde a {}ºF'.format(c, f))
+
+print('='*20)
