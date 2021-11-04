1b7718eb86c6b112cd327dd6b582177724dc1517
QuodGuido <50514473+QuodGuido@users.noreply.github.com>
2021-11-04 08:49:50 -0300

    ex013

---
+++ ex013.py
@@ -0,0 +1,9 @@
+print('='*20)
+print('Desafio 0013\nFaça um algoritmo que leia o salário de um funcionário e mostre seu novo salário,\n com 15% de aumento.')
+print('='*20)
+
+salario = float (input('Meu salario é: '))
+
+print('Meu salario é R${}, com o aumento de 15%, meu novo salario agora será R${}'.format(salario, salario * 0.15 + salario))
+
+print('='*20)
