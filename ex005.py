35facd60c320b06bc6043075b098bb9a333d75db
QuodGuido <50514473+QuodGuido@users.noreply.github.com>
2021-11-04 07:55:33 -0300

    ex005

---
+++ ex005.py
@@ -0,0 +1,8 @@
+print('='*20)
+print('Desafio 005\nFaça um programa que leia um número inteiro e mostre na tela seu sucessor e seu antecessor')
+print('='*20)
+
+num = int (input ('Escolha um número: '))
+print (f'Meu número é {num}, seu antecessor é {num-1} e seu sucessor é {num+1}')
+
+print('='*20)
